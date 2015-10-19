89ca91996819a77ef0bb4aec1259dd394cdce46d
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-19 08:36:32 -0500

    comenzar con listas simplemente ligadas

---
+++ listas/operaciones.c
@@ -0,0 +1,22 @@
+#include "operaciones.h"
+
+void borrar_lista(struct nodo cabecera)
+{
+	struct nodo *actual;
+	struct nodo *borrar;
+
+	/* Si es diferente de nulo borrar lista */
+	if (cabecera.sig != NULL) {
+		actual = cabecera.sig;
+		borrar = cabecera.sig;
+		/* Mientras el siguiente sea diferente de nulo */
+		while ( actual->sig != NULL) {
+			actual = actual->sig;
+			free(borrar);
+			borrar = actual;
+		}
+		free(borrar);
+		cabecera.sig = NULL;
+	}
+}
+
