5a325887ba2cae5bf82a9d8ba11b2f9d47c7690f
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-20 07:52:50 -0500

    Agregar funcion insertar al principio

--- listas/operaciones.c
+++ listas/operaciones.c
@@ -1,5 +1,16 @@
 #include "operaciones.h"
 
+void insertar_al_principio(struct nodo *cabecera, int valor_nuevo)
+{
+	struct nodo *nuevo;
+
+	nuevo = (struct nodo*) malloc(sizeof(struct nodo) * 1);
+	nuevo->val = valor_nuevo; 
+
+	nuevo->sig = cabecera->sig;
+	cabecera->sig = nuevo;
+}
+
 void borrar_lista(struct nodo *cabecera)
 {
 	struct nodo *actual;
