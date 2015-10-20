0581bba8e74424364ed7839e1f7fe2edaaff59d3
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-10-20 07:41:49 -0500

    Arreglar parametro por referencia
    La cabecera tiene que pasarse como parametro por referencia y no como
    parametro por valor
    
    Signed-off-by: Miguel Bernal Marin <mbernal@itszapopan.edu.mx>

--- listas/operaciones.c
+++ listas/operaciones.c
@@ -1,14 +1,14 @@
 #include "operaciones.h"
 
-void borrar_lista(struct nodo cabecera)
+void borrar_lista(struct nodo *cabecera)
 {
 	struct nodo *actual;
 	struct nodo *borrar;
 
 	/* Si es diferente de nulo borrar lista */
-	if (cabecera.sig != NULL) {
-		actual = cabecera.sig;
-		borrar = cabecera.sig;
+	if (cabecera->sig != NULL) {
+		actual = cabecera->sig;
+		borrar = cabecera->sig;
 		/* Mientras el siguiente sea diferente de nulo */
 		while ( actual->sig != NULL) {
 			actual = actual->sig;
@@ -16,7 +16,7 @@ void borrar_lista(struct nodo cabecera)
 			borrar = actual;
 		}
 		free(borrar);
-		cabecera.sig = NULL;
+		cabecera->sig = NULL;
 	}
 }
 
--- listas/operaciones.h
+++ listas/operaciones.h
@@ -3,14 +3,14 @@
 
 #include "nodo.h"
 
-void insertar_al_principio(struct nodo cabecera, int valor_nuevo);
+void insertar_al_principio(struct nodo *cabecera, int valor_nuevo);
 
-void insertar_al_final(struct nodo cabecera, int valor_nuevo);
+void insertar_al_final(struct nodo *cabecera, int valor_nuevo);
 
-void insertar_despues_de(struct nodo cabecera, int valor_nuevo, int valor_antes);
+void insertar_despues_de(struct nodo *cabecera, int valor_nuevo, int valor_antes);
 
-void imprimir_lista(struct nodo cabecera);
+void imprimir_lista(struct nodo *cabecera);
 
-void borrar_lista(struct nodo cabecera);
+void borrar_lista(struct nodo *cabecera);
 
 #endif
